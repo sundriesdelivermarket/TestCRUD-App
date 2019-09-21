946218649e03c8a9be1f66858edd2a421f7172ad
AJINKYA <angular.login3@gmail.com>
2019-09-21 22:51:17 +0530

    Add DBLogger Solution

---
+++ SimpleCRUD-API/DBLogger/AppRepository/DBManager.cs
@@ -0,0 +1,45 @@
+﻿using DBLogger.DBModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBLogger.AppRepository
+{
+    public class DBManager
+    {
+        private readonly MyModelContainer _dbContext;
+
+        public EmployeeRepository EmployeeRepository;
+
+        public DBManager(MyModelContainer context)
+        {
+            _dbContext = new MyModelContainer();
+            EmployeeRepository = new EmployeeRepository(context);
+        }
+
+        public DBManager(string connectionString)
+        {
+            _dbContext = new MyModelContainer(connectionString);
+            EmployeeRepository = new EmployeeRepository(connectionString);
+        }
+        
+
+        public void SaveEmployee(Employee employee)
+        {
+            EmployeeRepository.SaveEmployee(employee);
+        }
+
+        public IEnumerable<Employee> GetEmployees()
+        {
+            return EmployeeRepository.GetEmployees();
+        }
+
+        public Employee GetEmployeeById(int id)
+        {
+            return EmployeeRepository.GetEmployeeById(id);
+        }
+
+    }
+}
